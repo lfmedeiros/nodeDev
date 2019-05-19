72a7f5c9e65aa2fcf1245963096edf0995b2c220
lfmedeiros <lucamede@gmail.com>
2019-05-19 13:27:51 -0300

    Add

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 console.log('Server.js');
 const express = require ('express');
-const plotly = require ('Plotly');
 const knex = require ('Knex');
-
-console.log('Add Knex');
-console.log ('Add Sudo Permission');
-console.log('Not Have Permission');
-console.log('C');
-
 
 
 
